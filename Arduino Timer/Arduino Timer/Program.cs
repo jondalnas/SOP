2e99bcc785db139a47a5d5765e92dc69aa632925
Jonas Jensen <jdudal11@gmail.com>
2020-03-02 19:21:04 +0100

    Automatic time taking and writing data to CSV

--- Arduino Timer/Arduino Timer/Program.cs
+++ Arduino Timer/Arduino Timer/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO.Ports;
 using System.Threading;
+using System.IO;
 
 namespace Arduino_Timer {
 	class Program {
@@ -25,27 +26,44 @@ namespace Arduino_Timer {
 			int intPortChoice;
 			if (!Int32.TryParse(portChoice, out intPortChoice)) goto port;
 
-			baud:
+			/*baud:
 			Console.WriteLine("What baud rate? ");
 
 			string baudChoice = Console.ReadLine();
 
 			int intBaudChoice;
-			if (!Int32.TryParse(baudChoice, out intBaudChoice)) goto baud;
+			if (!Int32.TryParse(baudChoice, out intBaudChoice)) goto baud;*/
 
-			port = new SerialPort(ports[intPortChoice], intBaudChoice, Parity.None, 8, StopBits.One);
+			port = new SerialPort(ports[intPortChoice], 300, Parity.None, 8, StopBits.One);
 			port.Open();
 
-			uint samples = 1000;
-			var sw = System.Diagnostics.Stopwatch.StartNew();
-			for (int i = 0; i < samples; i++) {
-				port.Write(new byte[] { 170 }, 0, 1);
-				port.ReadByte();
+			StringBuilder csv = new StringBuilder();
+
+			uint[] bauds = { 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 76800, 115200, 230400, 250000, 500000 };
+			for (int baudI = 1; baudI < 17; baudI++) {
+				uint baud = bauds[baudI - 1];
+
+				uint samples = baud;
+				var sw = System.Diagnostics.Stopwatch.StartNew();
+				for (int i = 0; i < samples; i++) {
+					port.Write(new byte[] { 170 }, 0, 1);
+					port.ReadByte();
+				}
+				sw.Stop();
+				Console.WriteLine(baud + "," + ((sw.ElapsedMilliseconds / 1000.0) / samples));
+
+				csv.AppendLine(baud + "," + ((sw.ElapsedMilliseconds / 1000.0) / samples));
+
+				port.Close();
+				port.BaudRate = (int)bauds[baudI];
+				port.Open();
 			}
-			sw.Stop();
-			Console.WriteLine("It took " + (sw.ElapsedMilliseconds / 1000.0) + " seconds");
+
+			File.WriteAllText("Data.csv", csv.ToString());
 
 			Console.ReadLine();
+
+			port.Close();
 		}
 	}
 }
--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,9 +1,17 @@
+#include <SoftwareSerial.h>
+
 void setup() {
-  Serial.begin(9600);
 }
 
+uint32_t bauds[] = {300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 76800, 115200, 230400, 250000, 500000};
 void loop() {
-  while (Serial.available()) {
-    Serial.print((char) Serial.read());
+  for (uint8_t baudI = 0; baudI < 16; baudI++) {
+    uint32_t baud = bauds[baudI];
+    Serial.flush();
+    Serial.begin(baud);
+    for (uint32_t i = 0; i < baud; i++) {
+      while(!Serial.available());
+      Serial.print((char) Serial.read());
+    }
   }
 }
