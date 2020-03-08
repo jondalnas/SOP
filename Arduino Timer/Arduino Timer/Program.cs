cf8c9a913c554c80ec5219114eba5aae3eb0c6b9
Jonas Jensen <jdudal11@gmail.com>
2020-03-08 17:26:25 +0100

    Added comments to code

--- Arduino Timer/Arduino Timer/Program.cs
+++ Arduino Timer/Arduino Timer/Program.cs
@@ -12,6 +12,7 @@ namespace Arduino_Timer {
 		public static SerialPort port;
 
 		static void Main(string[] args) {
+			//Get list of serial prots on computer and print them
 			string[] ports = SerialPort.GetPortNames();
 
 			for (int i = 0; i < ports.Length; i++) {
@@ -21,66 +22,73 @@ namespace Arduino_Timer {
 			port:
 			Console.WriteLine("Which port? ");
 
+			//Read users choice, if it isn't a number, then ask for their choice again
 			string portChoice = Console.ReadLine();
 
 			int intPortChoice;
 			if (!Int32.TryParse(portChoice, out intPortChoice)) goto port;
 
-			/*baud:
-			Console.WriteLine("What baud rate? ");
-
-			string baudChoice = Console.ReadLine();
-
-			int intBaudChoice;
-			if (!Int32.TryParse(baudChoice, out intBaudChoice)) goto baud;*/
-
+			//Setup new port, with the same settings as the Arduino and open it
 			port = new SerialPort(ports[intPortChoice], 9600, Parity.None, 8, StopBits.One);
 			port.Open();
-
-			string csv = "Baud,2 byte (sec)\n";
 			
-			uint[] bauds = { 9600, 14400, 19200, 38400, 57600, 76800, 115200, 230400, 250000, 500000, 1000000, 2000000 };
+			//Create or override contens of Data.csv file
+			File.WriteAllText("Data.csv", "Baud,2 bytes (sec)\n");
+
+			//Go through list of baud rates and perform test
+			uint[] bauds = { 9600, 14400, 19200, 38400, 57600, 76800, 115200, 230400, 250000, 500000, 1000000, 2000000, 4000000, 8000000, 16000000 };
 			for (int baudI = 0; baudI < 12; baudI++) {
 				uint baud = bauds[baudI];
+				//Check if current baudrate is the same as the Arduino's
 				Handshake(baudI, baud);
 
-				Console.Write(baud);
-				csv += baud;
-
-				uint samples = baud<<2;
+				uint samples = baud;
+				uint frac = samples/10;
 
 				char chr;
 				uint curr = 0;
+				uint i = 0;
 				var sw = System.Diagnostics.Stopwatch.StartNew();
-				while ((chr = (char)port.ReadChar()) != 0) {
+				//Recieve character from Arduino, if it's a stop-byte or if it has recieved the right amount of bytes, then jump out of while-loop
+				while ((chr = (char)port.ReadChar()) != 0 && i < samples) {
+					//Print current progress
+					if (i % frac == 0) Console.WriteLine(baud + ", " + (i/(float)samples*100.0f) + "%");
+
+					//Calculate next nuber in the series
 					curr = (curr % 9) + 1;
 
+					//If the next number doesn't match the calculated number, then print an error message
 					if (curr != (int)chr) {
 						Console.WriteLine(baud + ", E: " + (int)chr);
 						curr = (uint)chr;
 					}
+
+					i++;
 				}
+				//Stop the stopwatch and write the baud rate and time to the CSV-file
 				sw.Stop();
 
-				Console.Write("," + ((sw.ElapsedMilliseconds / 1000.0) / samples) + "\n");
-				csv += "," + ((sw.ElapsedMilliseconds / 1000.0) / samples) + "\n";
+				Console.Write(baud + "," + ((sw.ElapsedMilliseconds / 1000.0) / samples) + "\n");
+				File.AppendAllText("Data.csv", baud + "," + ((sw.ElapsedMilliseconds / 1000.0) / samples) + "\n");
 			}
 
 			port.Close();
 
-			File.WriteAllText("Data.csv", csv.ToString());
-
 			Console.ReadLine();
 		}
 
+		//Check if Arduino and computer have the same baud rate and make sure we don't recieve data from the Arduino at the wrong baudrate
 		private static void Handshake(int baudI, uint baud) {
+			//Recieve bytes from Arduino, if it doesn't equal our baud rate, then try to read the next one
 			while (port.ReadByte() != baudI);
 
+			//Remove everyting from in buffer, so it isn't wrong data and setup the new baudrate
 			port.DiscardInBuffer();
 			port.Close();
 			port.BaudRate = (int)baud;
 			port.Open();
 
+			//Write the current baud rate back to the Arduino, to signal it to continue
 			port.Write(new Byte[] { (byte)baudI }, 0, 1);
 		}
 	}
--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,32 +1,73 @@
-#include <SoftwareSerial.h>
+#define SOFTWARE 0
+
+#if SOFTWARE
+  #include <SoftwareSerial.h>
+  
+  SoftwareSerial serial(0, 1);
+#endif
 
 void setup() {
-  Serial.begin(9600);
+  //Setup initial communication
+  #if SOFTWARE
+    serial.begin(9600);
+  #else
+    Serial.begin(9600);
+  #endif
 }
 
-uint32_t bauds[] = {9600, 14400, 19200, 38400, 57600, 76800, 115200, 230400, 250000, 500000, 1000000, 2000000};
+uint32_t bauds[] = {9600, 14400, 19200, 38400, 57600, 76800, 115200, 230400, 250000, 500000, 1000000, 2000000, 4000000, 8000000, 16000000};
 void loop() {
-    
+  //Loop through all 12 baud rates and test them
   for (uint8_t baudI = 0; baudI < 12; baudI++) {
     uint32_t baud = bauds[baudI];
-    
+
+    //Make sure that the program is listening at the right frequency
     handshake(baudI, baud);
-    
-    for (uint32_t i = 0; i < baud<<2; i++) {
-      Serial.print((char) (i % 9 + 1));
+
+    //Test each baud rate for n*4 times, where n is the baud rate,
+    //so it doesn't take too little time to finish a baud rate test,
+    //for the C# program to be able to take time
+    for (uint32_t i = 0; i < baud; i++) {
+      //Write 1-9 in binary
+      #if SOFTWARE
+        serial.write((byte) (i % 9 + 1));
+      #else
+        Serial.write((byte) (i % 9 + 1));
+      #endif
+      //Delay the program for a set amount of time, to simulate another process running
+      //delay(1);
     }
-    Serial.print((char) 0);
+
+    //Send a stop byte, so the program C# program knows to make the next readings
+    #if SOFTWARE
+      serial.write((byte) 0);
+    #else
+      Serial.write((byte) 0);
+    #endif
   }
   
   while(1);
 }
 
+//Handshake makes sure that the Arduino and C# program is in sync and that they don't send data at the wrong baud rate
 void handshake(uint8_t baudI, uint32_t baud) {
-  Serial.write((byte*)&baudI, 1);
+  //Write the current baud rate index, so the C# program knows where we are in the list
+  //Then delete any old messages recieved from the C# program and begin a new connection at the new baud rate
+  //Wait for the C# program to respond with the current baud rate index at the agreed baud rate
+  #if SOFTWARE
+    serial.write((byte*)&baudI, 1);
     
-  Serial.flush();
-  Serial.begin(baud);
-  
-  while (Serial.read() != baudI);
+    serial.flush();
+    serial.begin(baud);
+    
+    while (serial.read() != baudI);
+  #else
+    Serial.write((byte*)&baudI, 1);
+    
+    Serial.flush();
+    Serial.begin(baud);
+    
+    while (Serial.read() != baudI);
+  #endif
 }
 
